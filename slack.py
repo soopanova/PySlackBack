19338e4914d829101571636e2026429f1afb43d4
lab2112 <brandon.coates@gmail.com>
2016-05-09 00:08:23 -0400

    added test token method

--- slack.py
+++ slack.py
@@ -1,4 +1,5 @@
 import requests
+from pprint import pprint
 def get_key():
     with open('token.txt', 'r') as f:
         line = f.read()
@@ -9,9 +10,21 @@ def make_call(key, call):
     r = requests.get('http://slack.com/api/'+call, params=payload)
     print r.json()
 
+def test_token(token):
+    payload = {'token':token}
+    r = requests.get('https://slack.com/api/auth.test', params=payload)
+    results = r.json()
+    #print results['ok']
+    
+    if results['ok'] == True:
+        print 'Token is valid'
+    else:
+        print 'Token is invalid'
+
 def main():
     key = get_key()
     #print key
-    make_call(key, 'channels.list')
+    #make_call(key, 'channels.list')
+    test_token(key)
 if __name__ == '__main__':
     main()
